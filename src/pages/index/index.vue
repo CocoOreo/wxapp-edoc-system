aad8e6088e1c2be72f2394a5ab909ca16f57aa2f
CocoOreo <songhaoranuestc@163.com>
2021-02-21 13:26:20 +0800

    chore:完成Vant的引入以及基础构建

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -22,7 +22,11 @@
     </form>
 
     <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <div class="all">
         <div class="left">
         </div>
